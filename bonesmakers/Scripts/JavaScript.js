62142985944f56c9c76cd59b88b4a66e70b0446e
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2023-02-06 15:29:46 +0100

    fik lavet noget logik til at skabe kort efter de dataer de får, men der er ikke nogle måde at tilføre flere kort eller under kategorier, samt magnler jeg også en masse i forhold til at gemmen data og updater. Jeg skal finde en måde at lave en updatering uden at påvirker brugen og hold dataen updateret i databasen

--- bonesmakers/Scripts/JavaScript.js
+++ bonesmakers/Scripts/JavaScript.js
@@ -1,1 +1,7 @@
-﻿
+﻿const boxes = document.querySelectorAll('.selectType');
+
+boxes.forEach(box => {
+    box.addEventListener('change', function () {
+        this.parentElement.parentElement.className = this.value;
+    });
+});
